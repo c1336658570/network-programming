2a2579655b363a63354779e6bd46c5a1df3907c9
c1336658570 <1336658570@qq.com>
2022-06-01 23:02:51 +0800

    network study

--- TCP_IP_network_programming/chap18/18.51chat_server.c
+++ TCP_IP_network_programming/chap18/18.51chat_server.c
@@ -20,55 +20,47 @@ void error_handling(char *msg);
 
 int clnt_cnt = 0;
 int clnt_socks[MAX_CLNT];
-pthread_mutex_t mutex;
+pthread_mutex_t mutx;
 
 int main(int argc, char *argv[])
 {
     int serv_sock, clnt_sock;
     struct sockaddr_in serv_addr, clnt_addr;
     int clnt_addr_len;
-    pthread_t tid;
-
+    pthread_t t_id;
     if (argc != 2)
     {
-        printf("Usage : %s <port> \n", argv[0]);
+        printf("Usage : %s <port>\n", argv[0]);
         exit(1);
     }
 
-    pthread_mutex_init(&mutex, NULL);
-
+    pthread_mutex_init(&mutx, NULL);
     serv_sock = socket(PF_INET, SOCK_STREAM, 0);
 
-    bzero(&serv_addr, sizeof(serv_addr));
+    memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(atoi(argv[1]));
 
     if (bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
-    {
         error_handling("bind() error");
-    }
-
     if (listen(serv_sock, 5) == -1)
-    {
         error_handling("listen() error");
-    }
 
     while (1)
     {
         clnt_addr_len = sizeof(clnt_addr);
         clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_len);
 
-        pthread_mutex_lock(&mutex);
+        pthread_mutex_lock(&mutx);
         clnt_socks[clnt_cnt++] = clnt_sock;
-        pthread_mutex_unlock(&mutex);
+        pthread_mutex_unlock(&mutx);
 
-        pthread_create(&tid, NULL, handle_clnt, (void *)&clnt_sock);
-        pthread_detach(tid);
+        pthread_create(&t_id, NULL, handle_clnt, (void *)&clnt_sock);
+        pthread_detach(t_id);
         printf("Connected client IP: %s \n", inet_ntoa(clnt_addr.sin_addr));
     }
     close(serv_sock);
-
     return 0;
 }
 
@@ -78,13 +70,11 @@ void *handle_clnt(void *arg)
     int str_len = 0, i;
     char msg[BUF_SIZE];
 
-    while (str_len = read(clnt_sock, msg, sizeof(msg)) != 0)
-    {
+    while ((str_len = read(clnt_sock, msg, sizeof(msg))) != 0)
         send_msg(msg, str_len);
-    }
 
-    pthread_mutex_lock(&mutex);
-    for (i = 0; i < clnt_cnt; i++)
+    pthread_mutex_lock(&mutx);
+    for (i = 0; i < clnt_cnt; i++) // remove disconnected client
     {
         if (clnt_sock == clnt_socks[i])
         {
@@ -96,26 +86,21 @@ void *handle_clnt(void *arg)
         }
     }
     clnt_cnt--;
-    pthread_mutex_unlock(&mutex);
+    pthread_mutex_unlock(&mutx);
     close(clnt_sock);
-
     return NULL;
 }
-
-void send_msg(char *msg, int len)
+void send_msg(char *msg, int len) // send to all
 {
     int i;
-    pthread_mutex_lock(&mutex);
+    pthread_mutex_lock(&mutx);
     for (i = 0; i < clnt_cnt; i++)
-    {
         write(clnt_socks[i], msg, len);
-    }
-    pthread_mutex_unlock(&mutex);
+    pthread_mutex_unlock(&mutx);
 }
-
-void error_handling(char *buf)
+void error_handling(char *msg)
 {
-    fputs(buf, stderr);
+    fputs(msg, stderr);
     fputc('\n', stderr);
     exit(1);
 }
